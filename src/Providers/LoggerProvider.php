72cf0ef4b9b3f7210e8a1dcfbfc2ebd27bd58b0b
Graham Daniels <greydnls@gmail.com>
2017-10-27 16:06:37 -0400

    add in that sweet sweet logging

---
+++ src/Providers/LoggerProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Choredo\Providers;
+
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor;
+use Psr\Log\LoggerInterface;
+
+class LoggerProvider extends AbstractServiceProvider
+{
+    protected $provides = [LoggerInterface::class];
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     */
+    public function register()
+    {
+        $this->container->share(
+            LoggerInterface::class,
+            function () : LoggerInterface {
+                $handler = new StreamHandler('php://stdout');
+                $handler->setFormatter(new JsonFormatter());
+                $handler->pushProcessor(new Processor\WebProcessor());
+                $handler->pushProcessor(new Processor\MemoryUsageProcessor());
+                $handler->pushProcessor(new Processor\MemoryPeakUsageProcessor());
+                $handler->pushProcessor(new Processor\IntrospectionProcessor());
+                return new Logger('hma', [$handler]);
+            }
+        );
+    }
+}
