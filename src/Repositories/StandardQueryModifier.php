673836244041620f55b8681aba30da9567ca80bc
Graham Daniels <greydnls@gmail.com>
2017-11-23 12:33:16 -0500

    Abstract out StandardQueryModified

---
+++ src/Repositories/StandardQueryModifier.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Choredo\Repositories;
+
+
+use Choredo\LimitOffset;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class StandardQueryModifier
+{
+    public static function apply(QueryBuilder $queryBuilder, LimitOffset $pagination, array $sorts = [], array $filters = []) : Paginator
+    {
+        $rootAlias = array_pop($queryBuilder->getRootAliases());
+        foreach ($filters as $filter) {
+            $queryBuilder->andWhere(
+                $rootAlias.".{$filter->getField()} = :{$filter->getField()}"
+            )->setParameter($filter->getField(), $filter->getValue());
+        }
+
+        foreach ($sorts as $sort) {
+            $queryBuilder->orderBy(
+                $rootAlias.".{$sort->getField()}",
+                $sort->getDirection());
+        }
+
+        $paginator = new Paginator($queryBuilder);
+        $paginator->getQuery()
+            ->setFirstResult($pagination->getOffset())
+            ->setMaxResults($pagination->getLimit());
+
+        return $paginator;
+    }
+}
