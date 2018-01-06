4eb52ff0be000ada489162b5846c77814a92308d
Samantha Quiñones <ieatkillerbees@gmail.com>
2018-01-05 22:34:54 -0500

    Added validation test for Chores

---
+++ tests/unit/Entities/ChoreTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Choredo\Test\Entities;
+
+use Choredo\Entities\Chore;
+use Choredo\Entities\Family;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class ChoreTest extends TestCase
+{
+    /** @var Generator */
+    private $faker;
+
+    /** @var Family */
+    private $family;
+
+    public function setUp()
+    {
+        $this->faker  = Factory::create();
+        $this->family = $this->createMock(Family::class);
+    }
+
+    public function testValidationWithInvalidName()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('too long');
+        (new Chore(
+            Uuid::uuid4(),
+            $this->family,
+            str_repeat('a', 256),
+            ['sunday' => true]
+        ));
+    }
+
+    public function testValidationWithInvalidDescription()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('too long');
+        (new Chore(
+            Uuid::uuid4(),
+            $this->family,
+            $this->faker->firstName,
+            ['sunday' => true],
+            str_repeat('a', 256)
+        ));
+    }
+
+    public function testValidationWithInvalidScheduleKeys()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Schedule keys must be valid weekdays');
+        (new Chore(
+            Uuid::uuid4(),
+            $this->family,
+            $this->faker->firstName,
+            ['blorp' => true],
+            $this->faker->sentence()
+        ));
+    }
+
+    public function testValidationWithInvalidScheduleValues()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Schedule values must be true or false');
+        (new Chore(
+            Uuid::uuid4(),
+            $this->family,
+            $this->faker->firstName,
+            ['sunday' => 0],
+            $this->faker->sentence()
+        ));
+    }
+}
